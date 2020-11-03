d906a2be9289a1b0d1ee1f48e388b66228b9d506
AntoninaY01 <a.yordanova01@icloud.com>
2020-11-03 16:55:03 +0000

    2

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -2,8 +2,23 @@ package exercise6;
 
 import java.util.Scanner;
 
+import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
+
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter number: ");
+
+        int p = input.nextInt();
+        assert p > 0;
+        
+          int reversedNumber = 0;
+          int manipulatedNumber = p;
+          while (manipulatedNumber >0) {
+            reversedNumber = (reversedNumber * 10) + (manipulatedNumber % 10);
+            manipulatedNumber /=10;
+          }
+         boolean palindrome = p == reversedNumber;
+         System.out.println("Palindrome: " + palindrome);
     }
 }
